f5909c366aee46c7aa3dc598ed70a09c1027a5f2
Darkstrike <sayantandas579pc@gmail.com>
2025-07-30 20:24:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header';
 import Upload from './components/Upload';
 import Background from './components/Background';
 import Download from './components/Download';
+import Ghostlink from './components/Ghostlink';
+import Room from './components/Room';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Route path="/" element={<Background/>}/>
       <Route path="/Upload" element={<Upload/>}/>
       <Route path="/Download" element={<Download/>}/>
+      <Route path="/Ghostlink" element={<Ghostlink/>}/>
+      <Route path="/Room" element={<Room/>}/>
       </>
     </Routes>
     </Router>
